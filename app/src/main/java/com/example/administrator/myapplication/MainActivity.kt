c78c092311ca1d9589ef3b75b1fa85497b17f726
11Zero <874612467@qq.com>
2017-06-20 23:24:29 +0800

    添加线程，但是奇怪，为啥ui还是会阻塞

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.kt
@@ -9,11 +9,56 @@ import android.widget.Toast
 import com.example.administrator.myapplication.Presenter.UserPresenter
 import com.example.administrator.myapplication.View.IUserView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
+import java.lang.Math
+
 
 class MainActivity : AppCompatActivity(),IUserView, View.OnClickListener {
 
     private var mUserPresenter:UserPresenter?=null
-    private var mythread:UiThread?=null
+    private var msg:Int = 0
+//    private var thread :Thread? = null
+//    private var handler :android.os.Handler =object : android.os.Handler(){     //此处的object 要加，否则无法重写 handlerMessage
+//        override fun handleMessage(msg: android.os.Message?) {
+//            super.handleMessage(msg)
+//            if(msg?.what == 0){
+//                Thread.sleep(4000)
+//                editText_id.setText(Math.random().toString())
+//                //Toast.makeText(applicationContext,"子线程消息",Toast.LENGTH_LONG).show()
+//            }
+//        }
+//    }
+//    private fun myTherad(){
+//        thread = Thread(Runnable {
+//            kotlin.run {
+//                var message = android.os.Message()
+//                message.what = 0
+//                handler.sendMessage(message)
+//            }
+//        })
+//    }
+        private var mythread:Thread = thread(true){
+        var msg_id:Int = msg
+        run({
+            //editText_id.setText(Math.random().toString())
+            when(msg_id)
+            {
+                1->{
+                    Thread.sleep(4000)
+                    editText_lname.setText(Math.random().toString())
+                    msg_id = 0
+
+                }
+                2->{
+                    Thread.sleep(4000)
+                    editText_fname.setText(Math.random().toString())
+                    msg_id = 0
+                }
+                else->{}
+            }
+
+        })
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +67,6 @@ class MainActivity : AppCompatActivity(),IUserView, View.OnClickListener {
         //textView_fname.setText("Hello, guy!")
         progressBar_loading.max=100
         progressBar_loading.setProgress(20)
-
         btn_save.setOnClickListener(this)
         btn_load.setOnClickListener(this)
 //        btn_save.setOnClickListener({
@@ -34,8 +78,14 @@ class MainActivity : AppCompatActivity(),IUserView, View.OnClickListener {
     override fun onClick(v: View) {
 
         when(v.id){
-            R.id.btn_save->mUserPresenter?.saveUser(getID(),getFirstName(),getLastName())
-            R.id.btn_load->mUserPresenter?.loadUser(getID())
+            R.id.btn_save->{
+                msg = 1
+              mythread.run()
+                }//mUserPresenter?.saveUser(getID(),getFirstName(),getLastName())
+            R.id.btn_load->{
+                msg=2
+                mythread.run()
+                }//mUserPresenter?.loadUser(getID())
             else->{}
         }
     }
